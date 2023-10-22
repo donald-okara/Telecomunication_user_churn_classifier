0e5cde3d2d4ae0689b5800872cb20223cd372006
donald-okara <donaldokara123@gmail.com>
2023-10-22 17:26:12 +0300

    Feature importance added

--- Dashboard.py
+++ Dashboard.py
@@ -5,12 +5,21 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 import plotly.graph_objs as go
 import pandas as pd
+from plotly.subplots import make_subplots
+import plotly.graph_objs as go
+import plotly.io as pio
+import numpy as np
+import plotly.figure_factory as ff  # Import the violin module
+
+
 
 # Initialize the Dash app
 app = dash.Dash(__name__)
 
 # Read your data
 df = pd.read_csv('telecom_customer_churn.csv')  # Replace 'your_file.csv' with your CSV file path
+
+df['Customer Status'] = np.where((df['Customer Status'] != 'Churned'), 'Not Churned', df['Customer Status'])
 
 # Separate numerical and categorical columns
 from preprolib import myfunctions
@@ -28,6 +37,108 @@ num_cols = [x for x in num_cols if x not in remove_num]
 remove_cat = ['Gender', 'Married', 'Multiple Lines']
 cat_cols = [x for x in cat_cols if x not in remove_cat]
 
+
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+
+# Define the features and label
+features = cat_cols + num_cols
+label = 'Customer Status'
+
+# Convert the label column to ordinal categories
+label_encoder = OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1)
+
+y = label_encoder.fit_transform(df[label].values.reshape(-1, 1))
+
+# Reshape the y variable using ravel
+y = y.ravel()
+
+
+# Split the dataset into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(df[features], y, test_size=0.3, random_state=0)
+
+# Define a pipeline for numeric columns
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='mean')),
+    ('scaler', StandardScaler())
+])
+
+# Define a pipeline for categorical columns
+cat_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='most_frequent')),
+    ('encoder', OrdinalEncoder(handle_unknown='use_encoded_value', unknown_value=-1))
+])
+
+# Create a ColumnTransformer to apply the pipeline to the numeric and categorical columns
+preprocessor = ColumnTransformer(transformers=[
+    ('num', numeric_transformer, num_cols),
+    ('cat', cat_transformer, cat_cols)
+])
+
+# Fit the preprocessor to the training data and transform both the training and test data
+X_train_transformed = preprocessor.fit_transform(X_train)
+X_test_transformed = preprocessor.transform(X_test)
+
+
+# Create a dictionary to map encoded variables to original labels
+label_mapping = {}
+for i, label in enumerate(label_encoder.categories_[0]):
+    label_mapping[i] = label
+
+
+
+#Import classifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+
+rf_model = RandomForestClassifier(random_state=1).fit(X_train_transformed, y_train)  # Convert one-hot encoded y_train to 1D array
+
+
+#Model evaluation
+#from preprolib.myfunctions import evaluate_model
+
+#evaluate_model(rf_model,X_test_transformed, y_test)
+
+
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+    
+
+predictions = rf_model.predict(X_test_transformed)
+
+# Calculate feature importance using a trained RandomForestClassifier
+feature_importance = rf_model.feature_importances_
+
+# Combine feature names and their importance scores
+feature_importance_data = pd.DataFrame({'Feature': features, 'Importance': feature_importance})
+
+# Sort the features by importance (descending order)
+feature_importance_data = feature_importance_data.sort_values(by='Importance', ascending=False)
+
+
+
+
+#evaluate_model(rf_model,X_test_transformed, y_test)
+
+
+
+# Reshape the test_pred array to a 2D array
+predictions = predictions.reshape(-1, 1)
+
+predictions = label_encoder.inverse_transform(predictions)
+
+
+# Reshape the test_pred array to a 2D array
+y_test = y_test.reshape(-1, 1)
+
+y_test = label_encoder.inverse_transform(y_test)
+y_test
+
+# Create a placeholder for the confusion matrix plot
+confusion_matrix_plot = dcc.Graph(id='confusion-matrix-plot')
+
 # Define the layout of the app
 app.layout = html.Div([
     html.H1(id='H1', children='Telecommunication User Churn Dashboard', style={'textAlign': 'center',
@@ -35,7 +146,7 @@ app.layout = html.Div([
     
     # 1. Distribution of the Target Variable (Customer Status)
     dcc.Graph(id='status-distribution'),
-    
+
     # 2. Distribution of Other Columns (Histograms)
     html.Div(id='histogram-subplots'),
     
@@ -71,7 +182,31 @@ app.layout = html.Div([
     # Numeric feature comparison (Box plots)
     html.Div(id='box-plots-container', style={'display': 'flex', 'flex-wrap': 'wrap'}),
     
-    # Continue with your existing layout...
+    html.P(id='P1', children='This is the confusion matrix for the model.', style={'textAlign': 'left',
+                                                                      'marginTop': 20, 'marginBottom': 20}),
+
+    confusion_matrix_plot,
+# Inside the app.layout definition
+    dcc.Graph(id='feature-importance-plot'),
+
+    dcc.Markdown(
+        id='P2',
+        children='''Cross-validation Scores: 0.82344428  
+                     0.84057971  
+                     0.81884058  
+                     0.81014493  
+                     Average Cross-validation Accuracy: 0.8264279871641603''',
+        style={'textAlign': 'left', 'marginTop': 20, 'marginBottom': 20}
+    ),
+
+    dcc.Markdown(
+        id='P3',
+        children='''Model Evaluation: Accuracy: 0.8248816768086545  
+                     Precision: 0.8192510046389897  
+                     Recall: 0.8248816768086545  
+                     F1-score: 0.8207080313395132''',
+        style={'textAlign': 'left', 'marginTop': 20, 'marginBottom': 20}
+    ),
 ])
 
 # 1. Distribution of the Target Variable (Customer Status)
@@ -167,6 +302,56 @@ def update_pie_charts(feature, status1, status2):
 
     return pie_charts
 
+
+# Callback to update the confusion matrix plot
+@app.callback(
+    Output('confusion-matrix-plot', 'figure'),
+    Input('status1-dropdown', 'value'),
+    Input('status2-dropdown', 'value')
+)
+def update_confusion_matrix_plot(status1, status2):
+    # Calculate the confusion matrix as you did before
+    confusion = confusion_matrix(y_test, predictions)
+    confusion_df = pd.DataFrame(confusion, index=["Actual " + str(label) for label in np.unique(y_test)],
+                                columns=["Predicted " + str(label) for label in np.unique(y_test)])
+    
+    # Define the custom blue color scale
+    color_scale = [[0, '#FFFFFF'], [0.1, '#D9F0FF'], [0.5, '#77B2E5'], [1, '#003399']]
+    
+    # Create the confusion matrix heatmap
+    fig = px.imshow(confusion_df, title="Confusion Matrix", color_continuous_scale=color_scale)
+    fig.update_xaxes(title_text="Predicted Labels")
+    fig.update_yaxes(title_text="Actual Labels")
+    
+    return fig
+
+
+# Add this callback function at the end of your code
+
+@app.callback(
+    Output('feature-importance-plot', 'figure'),
+    Input('status1-dropdown', 'value'),
+    Input('status2-dropdown', 'value')
+)
+def update_feature_importance_plot(status1, status2):
+    # Calculate feature importance using a trained RandomForestClassifier
+    feature_importance = rf_model.feature_importances_
+
+    # Combine feature names and their importance scores
+    feature_importance_data = pd.DataFrame({'Feature': features, 'Importance': feature_importance})
+
+    # Sort the features by importance (descending order)
+    feature_importance_data = feature_importance_data.sort_values(by='Importance', ascending=False)
+
+    # Create a bar chart to visualize feature importance
+    fig = px.bar(
+        feature_importance_data,
+        x='Feature',
+        y='Importance',
+        title='Feature Importance',
+    )
+
+    return fig
 
 
 # Run the app
